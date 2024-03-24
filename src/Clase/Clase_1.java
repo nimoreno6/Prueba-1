9ab1a61ba0a1146bbc26f77e3de0591745916de0
programacion <nickmorenosf@gmail.com>
2024-03-24 14:27:38 -0700

    practica java

---
+++ src/Clase/Clase_1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clase;
+
+/**
+ *
+ * @author Nicolaspc
+ */
+public class Clase_1 {
+    public static void main(String args[])
+    {
+        for(int i = 0; i <= 22; i++)
+        {
+            System.out.print("22 * "+ i + " = " + 22 * i + "\n");
+        }
+    }
+    
+}
---
+++ src/galleta/galleta.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package galleta;
+
+/**
+ *
+ * @author Nicolaspc
+ */
+public class galleta {
+    
+    private String molde;
+    private String receta;
+    private String color;
+    
+    public Galleta()
+    {
+        //Constructor
+    }
+//hola mundo
+    /*
+    
+    */
+    public Galleta fabricar_galleta()
+    {
+        //Método para fabricar galletas
+    }
+}
+
